68ad3c7e8709cc1738559788b3b90bb03b824d9e
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-07 13:11:42 -0600

    feat: add mock data and images for test

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,9 +1,11 @@
+import { CustomPagination } from "@/components/custom/CustomPagination";
 import CustomJumbotron from "@/shop/components/CustomJumbotron";
 
 const HomePage = () => {
   return (
     <>
-      <CustomJumbotron  title="Todos los Productos"/>
+      <CustomJumbotron title="Todos los Productos" />
+      <CustomPagination totalPages={7} />
     </>
   );
 };
