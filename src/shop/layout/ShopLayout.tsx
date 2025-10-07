4dbefed78fc8c44f1bb72ece975fcb9322221ba7
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-10-07 13:01:12 -0600

    feat: add footer component from template

--- src/shop/layout/ShopLayout.tsx
+++ src/shop/layout/ShopLayout.tsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import CustomHeader from "../components/CustomHeader";
+import CustomFooter from "../components/CustomFooter";
 
 const ShopLayout = () => {
   return (
     <div className="min-h-screen bg-background">
       <CustomHeader />
       <Outlet />
+      <CustomFooter />
     </div>
   );
 };
