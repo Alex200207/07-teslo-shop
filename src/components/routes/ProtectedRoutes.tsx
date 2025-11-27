1f20532aa1fbc1fb3e0af6a605f9c274310fc1af
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-11-26 18:11:11 -0600

    feat: refactor ProtectedRoutes to improve code readability and structure; add AdminRoutes for admin-specific navigation

--- src/components/routes/ProtectedRoutes.tsx
+++ src/components/routes/ProtectedRoutes.tsx
@@ -11,18 +11,29 @@ export const AuthenticatedRoute = ({ children }: PropsWithChildren) => {
   if (authStatus === "not-authenticated")
     return <Navigate to="/auth/login" replace />;
 
-
   // si esta autenticado, renderizamos los hijos
   return children;
 };
 
 export const NotAuthenticatedRoute = () => {
-   // extraemos el estado de autenticación
+  // extraemos el estado de autenticación
   const { authStatus } = useAuthStore();
   // si el estado de autenciacion es checking no renderizamos nada
   if (authStatus === "checking") return null;
   // verficas si el auth estatus es igual a authenticated y redirigimos al home
-  if (authStatus === "authenticated")
-    return <Navigate to="/" replace />;
+  if (authStatus === "authenticated") return <Navigate to="/" replace />;
 };
 
+export const AdminRoutes = () => {
+  // extraemos el estado de autenticación y el isAdmin
+  const { authStatus, isAdmin } = useAuthStore();
+  // si el estado de autenciacion es checking no renderizamos nada
+  if (authStatus === "checking") return null;
+
+  //verficas si el auth estatus es igual a not-authenticated y redirigimos al login
+  if (authStatus === "not-authenticated")
+    return <Navigate to="/auth/login" replace />;
+
+  // verificamos si el usuario no es admin y redirigimos al home
+  if (!isAdmin()) return <Navigate to="/" replace />;
+};
