97909632bbdd4a7db190380d5aa8842277cde6c7
EDDY ALEXIS TALAVERA ESPINOZA <162833904+Alex200207@users.noreply.github.com>
2025-11-26 18:08:25 -0600

    feat: add AuthenticatedRoute and NotAuthenticatedRoute components for handling authentication-based navigation

---
+++ src/components/routes/ProtectedRoutes.tsx
@@ -0,0 +1,28 @@
+import { useAuthStore } from "@/auth/store/auth.store";
+import type { PropsWithChildren } from "react";
+import { Navigate } from "react-router";
+
+export const AuthenticatedRoute = ({ children }: PropsWithChildren) => {
+  // extraemos el estado de autenticación
+  const { authStatus } = useAuthStore();
+  // si el estado de autenciacion es checking no renderizamos nada
+  if (authStatus === "checking") return null;
+  // verficas si el auth estatus es igual a no-authenticated y redirigimos al login
+  if (authStatus === "not-authenticated")
+    return <Navigate to="/auth/login" replace />;
+
+
+  // si esta autenticado, renderizamos los hijos
+  return children;
+};
+
+export const NotAuthenticatedRoute = () => {
+   // extraemos el estado de autenticación
+  const { authStatus } = useAuthStore();
+  // si el estado de autenciacion es checking no renderizamos nada
+  if (authStatus === "checking") return null;
+  // verficas si el auth estatus es igual a authenticated y redirigimos al home
+  if (authStatus === "authenticated")
+    return <Navigate to="/" replace />;
+};
+
